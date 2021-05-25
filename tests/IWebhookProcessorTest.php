f6e8f50079d3f5c6aa5dc6615939e721572398b4
afsara-ben <1505118.ab@ugrad.cse.buet.ac.bd>
2021-05-25 13:22:55 +0600

    Adds tests for models and interface

---
+++ tests/IWebhookProcessorTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Dotlines\GhooriOnDemand\Tests;
+
+use Dotlines\GhooriOnDemand\Models\Webhook;
+use Dotlines\GhooriOnDemand\Models\WebhookResponse;
+use PHPUnit\Framework\TestCase;
+
+class IWebhookProcessorTest extends TestCase
+{
+    /**
+     * @test
+     */
+    final public function it_can_process_a_webhook_notification(): void
+    {
+        $webhookResponseObj = new WebhookResponse("sadasfdas");
+        $this->assertInstanceOf(WebhookResponse::class, $webhookResponseObj);
+    }
+}
